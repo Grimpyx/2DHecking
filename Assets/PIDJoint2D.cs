b1fae38c463771542156e0789b47b574b59ce7ae
Jesper Fagerberg <jesperfagerberg@live.se>
2022-10-03 14:37:45 +0200

    J: Testtrail

--- Assets/PIDJoint2D.cs
+++ Assets/PIDJoint2D.cs
@@ -6,16 +6,23 @@ public class PIDJoint2D : MonoBehaviour
 {
     public Transform followedBody;
     public Vector2 linOffset;
+    public bool adoptLinearVelocity = true;
+    public bool adoptAngularVelocity = true;
 
+    [Header("Linear:")]
+    public bool followPosition = true;
+    [SerializeField] PID xPID;
+    [SerializeField] PID yPID;
+    public float forcePID = 1f;
 
-    [SerializeField] PID xPID, yPID;
-    public float forcePID = 10f;
-
-
-    public bool followPosition, followRotation;
+    [Header("Angular:")]
+    public bool followRotation = true;
+    [SerializeField] PID rotPID;
+    public float forceRotPID = 1f;
 
 
     private Rigidbody2D rb;
+    private Rigidbody2D rbTarget;
 
 
     void Start()
@@ -31,21 +38,30 @@ public class PIDJoint2D : MonoBehaviour
         yPID.integralSaturation = 1;*/
 
         rb = GetComponent<Rigidbody2D>();
+        rbTarget = followedBody.GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        if(followPosition)
+        if (adoptLinearVelocity)
+            rb.velocity = rbTarget.velocity;
+
+        if (adoptAngularVelocity)
+            rb.angularVelocity = rbTarget.angularVelocity;
+
+        if (followPosition)
         {
             float x = xPID.Update(Time.fixedDeltaTime, transform.position.x, followedBody.position.x + linOffset.x);
             float y = yPID.Update(Time.fixedDeltaTime, transform.position.y, followedBody.position.y + linOffset.y);
 
-            rb.AddForce(Time.fixedDeltaTime * 0.01f * forcePID * new Vector2(x, y));
+            rb.AddForce(rb.mass * forcePID * new Vector2(x, y), ForceMode2D.Force);
         }
 
         if(followRotation)
         {
+            float angle = rotPID.UpdateAngle(Time.fixedDeltaTime, rb.rotation, rbTarget.rotation);
 
+            rb.AddTorque(rb.mass * forceRotPID * angle, ForceMode2D.Force);
         }
     }
 }
