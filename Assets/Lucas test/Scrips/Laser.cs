21c5e9ad35244e101253b42720f271370ece9465
Bushw0oki3 <xkandarfx@gmail.com>
2022-10-03 14:36:43 +0200

    L: sword temp prefab

--- Assets/Lucas test/Scrips/Laser.cs
+++ Assets/Lucas test/Scrips/Laser.cs
@@ -21,24 +21,24 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Mouse.current.leftButton.isPressed && !lineRenderer.enabled)
+        if(Mouse.current.leftButton.wasPressedThisFrame && !lineRenderer.enabled)
         {
             EnableLaser();
         }
 
         
 
-        else if(Mouse.current.leftButton.wasReleasedThisFrame && lineRenderer.enabled)
+        else if(Mouse.current.leftButton.wasPressedThisFrame && lineRenderer.enabled)
         {
             DisableLaser();
         }
 
-        RotateToMouse();
+        //RotateToMouse();
 
-        if (lineRenderer.enabled)
-        {
-            UpdateLaser();
-        }
+        //if (lineRenderer.enabled)
+        //{
+            //UpdateLaser();
+        //}
     }
 
     void EnableLaser()
@@ -49,21 +49,21 @@ public class Laser : MonoBehaviour
 
     void DisableLaser() => lineRenderer.enabled = false;
 
-    void UpdateLaser()
-    {
-        var mousePos = (Vector2)cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    //void UpdateLaser()
+    //{
+        //var mousePos = (Vector2)cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
-        lineRenderer.SetPosition(0, swordPoint.position);
+        //lineRenderer.SetPosition(0, swordPoint.position);
 
-        lineRenderer.SetPosition(1, mousePos);
-    }
+        //lineRenderer.SetPosition(1, mousePos);
+    //}
 
-    void RotateToMouse()
-    {
-        Vector2 direction = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rotation.eulerAngles = new Vector3(0, 0, angle);
-        transform.rotation = rotation;
-    }
+    //void RotateToMouse()
+    //{
+        //Vector2 direction = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rotation.eulerAngles = new Vector3(0, 0, angle);
+        //transform.rotation = rotation;
+    //}
 
 }
